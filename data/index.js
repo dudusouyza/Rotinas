eba5213c3030e7424a47aae6993b17e01186c460
Your Name <you@example.com>
2024-04-09 14:11:23 -0300

    arquivo dt

---
+++ data/index.js
@@ -0,0 +1,58 @@
+const DATA =
+
+[
+   {
+
+    id:'01',
+    porcentagem:'45%',
+    titulo:'Leitura',
+    Imagem:'https://www.redeicm.org.br/carmo/wp-content/uploads/sites/5/2019/01/Livro.jpg',
+    descrição:'A leitura estimula o raciocínio, melhora o vocabulário, aprimora a capacidade interpretativa, além de proporcionar ao leitor um conhecimento amplo e diversificado sobre vários assuntos.'
+
+   },
+
+   {
+    id:'02',
+    porcentagem:'45%',
+    titulo:'cinema',
+    Imagem:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.sbt.com.br%2Ffilmes%2Fcinema-em-casa&psig=AOvVaw04PK05tHULlBNUO75gNFPJ&ust=1711557361798000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLCIjpeukoUDFQAAAAAdAAAAABAE',
+    descrição:'Cinema é uma palavra que se refere à arte de produção de filmes. É também uma abreviação de cinematógrafo, o aparelho desenvolvido pelos irmãos Lumiére que gravava e projetava filmes.' 
+   },
+
+   {
+    id:'03',
+    porcentagem:'45%',
+    titulo:'comida',
+    Imagem:'https://cdn.abjnoticias.com.br/abjnoticias-home/2023/05/colesterol_comidas-1.jpg',
+    descrição:'A culinária é a arte de cozinhar ou confeccionar alimentos e foi evoluindo de acordo com a história da humanidade e possui características diferentes em cada cultura.' 
+   },
+
+   {
+    id:'04',
+    porcentagem:'45%',
+    titulo:'Informática',
+    Imagem:'https://s1.static.brasilescola.uol.com.br/be/conteudo/images/-5be0989bbc0c9.jpg',
+    descrição:'Entre os conhecimentos da informática básica, podemos destacar o conhecimento das peças fundamentais dos computadores (hardware), funcionamento de cabos, fios e conexões diversos.' 
+   },
+
+   {
+    id:'05',
+    porcentagem:'45%',
+    titulo:'Basquete',
+    Imagem:'https://regrasdoesporte.com.br/wp-content/uploads/2013/01/basquete3.jpg',
+    descrição:'O nome em inglês, basketball, significa bola no cesto.' 
+   },
+
+   {
+    id:'06',
+    porcentagem:'45%',
+    titulo:'volei',
+    Imagem:'https://yt3.googleusercontent.com/ytc/AIdro_m_qSkLi30YPgiV3RGDjZGJLU8tM77lnKIrQvbU=s900-c-k-c0x00ffffff-no-rj',
+    descrição:'A leitura estimula o raciocínio, melhora o vocabulário, aprimora a capacidade interpretativa, além de proporcionar ao leitor um conhecimento amplo e diversificado sobre vários assuntos.' 
+   },
+
+  
+
+
+]
+export default DATA;
